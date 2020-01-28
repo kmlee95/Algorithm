8b9fe1f388e5dfc56c768251afa529b91bf5cdbe
kmlee95 <kmlee95@naver.com>
2020-01-28 23:19:22 +0900

    20200128

---
+++ SW_Academy/SW_9088_다이아몬드.java
@@ -0,0 +1,41 @@
+package SW_Academy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class SW_9088_다이아몬드 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static void solve(int num) throws IOException {
+        int n, k;
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        k = Integer.parseInt(tokenizer.nextToken());
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arrayList.add(Integer.parseInt(reader.readLine()));
+        }
+        Collections.sort(arrayList);
+        int numMax = 0;
+        for (int i = 0; i < n; i++) {
+            int min = arrayList.get(i);
+            int max = min + k;
+            int thisMax = 0;
+            for (int j = 0; j < n; j++) {
+                if (min <= arrayList.get(j) && arrayList.get(j) <= max) {
+                    thisMax++;
+                }
+            }
+            numMax = Math.max(numMax,thisMax);
+        }
+        System.out.println("#"+num+" "+numMax);
+    }
+    public static void main(String args[]) throws IOException {
+        int T = Integer.parseInt(reader.readLine());
+        for(int test_case = 1; test_case <= T; test_case++){
+            solve(test_case);
+        }
+    }
+}
---
+++ SW_Academy/SW_9317_받아쓰기.java
@@ -0,0 +1,28 @@
+package SW_Academy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class SW_9317_받아쓰기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader r= new BufferedReader(new InputStreamReader(System.in)); 
+		int T = Integer.parseInt(r.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int sum = 0;
+			int N = Integer.parseInt(r.readLine());
+			String a = r.readLine();
+			String b = r.readLine();
+			
+			for(int i = 0; i < N;  i++) {
+				if(a.charAt(i) == b.charAt(i))
+                    sum++;
+			}
+			System.out.println("#" + test_case + " " + sum);
+		}
+	}
+}
