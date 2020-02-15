e412be06f325670f2ce4c020693c277a3e50deb1
이경민 <leekyungmin@leekyungmin.local>
2020-02-15 23:43:27 +0900

    배열순환

---
+++ SW_Academy/SW_8840.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class SW_8840 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(in.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            long L = Long.parseLong(in.readLine());
+            long num = (L / 2) * 1L;
+            System.out.println("#" + t + " " + num * num);
+        }
+    }
+}
