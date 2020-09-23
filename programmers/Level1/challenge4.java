9b04463114024a0e940a70412097336269d30397
kmlee95 <kmlee95@naver.com>
2020-09-23 12:22:51 +0900

    fix: level1 practice2

--- programmers/Level1/challenge4.java
+++ programmers/Level1/challenge4.java
@@ -5,39 +5,38 @@ import java.util.List;
 
 public class challenge4 {
     public static int[] solution(int[] answers) {
-        int[] result = { 0, 0, 0 };
         int[] answer = {};
-        int max = 0;
-        int[] person1 = { 1, 2, 3, 4, 5 };
-        int[] person2 = { 2, 1, 2, 3, 2, 4, 2, 1 };
-        int[] person3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+        int[] num_1 = { 1, 2, 3, 4, 5 };
+        int[] num_2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+        int[] num_3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+        int count_1 = 0;
+        int count_2 = 0;
+        int count_3 = 0;
+        int max_count = 0;
 
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == person1[i % 5])
-                result[0]++;
-            if (answers[i] == person2[i % 8])
-                result[1]++;
-            if (answers[i] == person3[i % 10])
-                result[2]++;
-        }
-        for (int k = 0; k < 3; k++) {
-            if (max < result[k])
-                max = result[k];
+            if (num_1[i % 5] == answers[i])
+                count_1++;
+            if (num_2[i % 8] == answers[i])
+                count_2++;
+            if (num_3[i % 10] == answers[i])
+                count_3++;
         }
 
+        max_count = Math.max(count_1, Math.max(count_2, count_3));
+
         List<Integer> list = new ArrayList<>();
-        if (max == result[0])
+        if (max_count == count_1)
             list.add(1);
-        if (max == result[1])
+        if (max_count == count_2)
             list.add(2);
-        if (max == result[2])
+        if (max_count == count_3)
             list.add(3);
 
         answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = (int) list.get(i);
         }
-
         return answer;
     }
 
---
+++ programmers/Level1/challenge5.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//같은 숫자는 싫어
+public class challenge5 {
+    public static int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        int[] answer = {};
+        int test = 10; // 0보다 크고 9보다 작은 정수이므로 최댓값 기록.
+        for (int values : arr) {
+            if (test != values)
+                list.add(values);
+            test = values;
+        }
+        answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).intValue();
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int arr1[] = { 1, 1, 3, 3, 0, 1, 1 };
+        int result[] = solution(arr1);
+        for (int data : result) {
+            System.out.print(" " + data);
+        }
+    }
+}
